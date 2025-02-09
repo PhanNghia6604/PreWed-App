5ddb28e47b68a7b2fe7ef47aa1bb92742b639e7c
Nhathaha1 <nhatnguyen281003@gmail.com>
2025-02-09 13:01:36 +0700

    update router

--- login-signup/src/App.js
+++ login-signup/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { LoginSignup } from './Components/Assets/LoginSignup/LoginSignup';
+import { Page } from './Components/Page/Page';
 
 function App() {
   return (
-    <div>
-     <LoginSignup/>
-    </div>
+    <>
+     <Page/>
+    </>
   );
 }
 
---
+++ login-signup/src/Components/Page/Page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import { Header } from '../Common/Header';
+
+export const Page = () => {
+  return (
+    <>
+      <Router>
+        <Header/>
+        
+
+      </Router>
+    </>
+  )
+}
