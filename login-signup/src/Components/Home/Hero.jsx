def8c66ffef620f9917af06a6f05e38ba3ce84f8
Nhathaha1 <nhatnguyen281003@gmail.com>
2025-02-10 12:36:39 +0700

    Giao diện Home

--- login-signup/src/Components/Home/Hero.jsx
+++ login-signup/src/Components/Home/Hero.jsx
@@ -7,13 +7,15 @@ export const Hero = () => {
     <>
        <section className='hero'>
             {home.map((val,i) => (
-                <div className="heroContainer">
+                <div className="heroContent">
                     <h3>{val.text}</h3>
                     <h1>
                         <Typewriter options ={{strings :  [`${val.name}`,`${val.post}`,`${val.design}`],
                         autoStart: true, loop: true
                     }} />
                     </h1>
+                    <p>{val.desc}</p>
+                    
                 </div>
             ))}
          </section> 
