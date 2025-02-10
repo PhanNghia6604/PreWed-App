c0a75458b009606d3134de79f1321bb7b1d0a15b
Nhathaha1 <nhatnguyen281003@gmail.com>
2025-02-10 13:52:31 +0700

    Cập nhật giao diện trang đếm số (Counter.jsx)

---
+++ login-signup/src/Components/Page/Counter.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { project } from '../fake data/data'
+import CountUp from 'react-countup'
+
+export const Counter = () => {
+  return (
+    <>  
+        <div className="hero counter">
+            <div className="container grid3 grid4">
+                {project.map((item) => (
+                    <div className="box">
+                        <i>{item.icon}</i>
+                        <h1 className='heading'>
+                            <CountUp enableScrollby duration={2} end ={item.num}/>
+                        </h1>
+                        <h3>{item.title}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </>
+  )
+}
