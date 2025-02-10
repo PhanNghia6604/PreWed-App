cb084c9da8cd9425f59123b22220e64d9f54736a
Nhathaha1 <nhatnguyen281003@gmail.com>
2025-02-10 12:53:52 +0700

    thêm giao diện cho trang about us

--- login-signup/src/Components/Home/Home.jsx
+++ login-signup/src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Hero} from "./Hero"
+import { About } from '../Page/About'
 
 export const Home = () => {
   return (
     <>
       <Hero/>
+      <About/>
     </>
   )
   
