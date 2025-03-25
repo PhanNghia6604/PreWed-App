41f02cd28b8d82fd8370f0f4393e4d9b5d7424e2
Nhathaha1 <nhatnguyen281003@gmail.com>
2025-03-25 12:34:43 +0700

    Add Admin Blogs

--- login-signup/src/Components/Common/AdminHeader.jsx
+++ login-signup/src/Components/Common/AdminHeader.jsx
@@ -32,7 +32,7 @@ export const AdminHeader = ({ isLoggedIn, setIsLoggedIn }) => {
                     <Link to="/admin-slots" className={styles.navLink}>Quản lý tạo slot</Link>
                     <Link to="/admin-servicepackage" className={styles.navLink}>Quản lý gói dịch vụ</Link>
                     <Link to="/admin-reports" className={styles.navLink}>Báo cáo thu nhập</Link>
-                  
+                    <Link to="/admin-blogs" className={styles.navLink}>Quản lý Blogs</Link>
 
                     {/* User Menu */}
                     {isLoggedIn && (
