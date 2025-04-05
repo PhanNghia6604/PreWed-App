29b636b0704cc3a0b3463fa8c35b196e8c00ccdf
PhanNghia6604 <Nghiapvse181706@fpt.edu.vn>
2025-04-05 16:03:08 +0700

    update UI , Test and home page

--- login-signup/src/Components/Home/Hero.jsx
+++ login-signup/src/Components/Home/Hero.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
-import {home} from '../fake data/data'
+import { home } from '../fake data/data'
+import { Link } from 'react-router-dom'
 import Typewriter from 'typewriter-effect'
 
 export const Hero = () => {
   return (
-    <>
-       <section className='hero'>
-            {home.map((val,i) => (
-                <div className="heroContent">
-                    <h3>{val.text}</h3>
-                    <h1>
-                        <Typewriter options ={{strings :  [`${val.name}`,`${val.post}`,`${val.design}`],
-                        autoStart: true, loop: true
-                    }} />
-                    </h1>
-                    <p>{val.desc}</p>
-                    
-                </div>
-            ))}
-         </section> 
-    </>
+    <section className='hero'>
+      {home.map((val, i) => (
+        <div className="heroContent" key={i}>
+          <h3>{val.text}</h3>
+          <h1>
+            <Typewriter 
+              options={{
+                strings: [`${val.name}`, `${val.post}`, `${val.design}`],
+                autoStart: true,
+                loop: true
+              }} 
+            />
+          </h1>
+          <p>{val.desc}</p>
+
+          {/* Liên kết đến trang "Premarital Test" */}
+          <Link to="/test" className="premaritalTestBtn">Premarital Test</Link>
+        </div>
+      ))}
+    </section>
   )
 }
