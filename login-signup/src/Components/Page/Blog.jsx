c55172eae9da6ddfcfb27f829ace1afae5df3016
BAO0806 <baopgse181724@fpt.edu.vn>
2025-04-07 13:29:12 +0700

    Fix CSS

--- login-signup/src/Components/Page/Blog.jsx
+++ login-signup/src/Components/Page/Blog.jsx
@@ -1,56 +1,59 @@
-import React, {useEffect} from 'react'
-import { Heading } from '../Common/Heading'
-import {blog} from '../fake data/data'
+import React, { useEffect } from 'react';
+import { Heading } from '../Common/Heading';
+import { blog } from '../fakeData/data';
 
 export const Blog = () => {
-    useEffect(() => {
-              // Hàm kiểm tra khi phần tử xuất hiện trong tầm nhìn
-              const handleScroll = () => {
-                const elements = document.querySelectorAll('.fade in');
-                elements.forEach(element => {
-                  const position = element.getBoundingClientRect().top;
-                  const windowHeight = window.innerHeight;
-          
-                  // Nếu phần tử trong cửa sổ, thêm class 'visible'
-                  if (position < windowHeight) {
-                    element.classList.add('visible');
-                  }
-                });
-              };
-              // Lắng nghe sự kiện cuộn trang
-              window.addEventListener('scroll', handleScroll);
-          
-              // Kiểm tra ngay khi trang được tải
-              handleScroll();
-              return () => {
-                window.removeEventListener('scroll', handleScroll);
-              };
-            }, []);
+  useEffect(() => {
+    // Hàm kiểm tra khi phần tử xuất hiện trong tầm nhìn
+    const handleScroll = () => {
+      const elements = document.querySelectorAll('.fade-in');
+      elements.forEach(element => {
+        const position = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Nếu phần tử trong cửa sổ, thêm class 'visible'
+        if (position < windowHeight) {
+          element.classList.add('visible');
+        }
+      });
+    };
+
+    // Lắng nghe sự kiện cuộn trang
+    window.addEventListener('scroll', handleScroll);
+
+    // Kiểm tra ngay khi trang được tải
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-        <section className='blog fade-in'>
-            <div className="container">
-                <Heading title='Blog'/>
-                <div className='content grid3'>
-                {blog.map((item) => {
-               return(
-                    <div className="box ">
-                        <div className="img">
-                            <img src ={item.cover} alt=''/>
-                        </div>
-                        <div className="text">
-                            <h3>{item.title}</h3>
-                            <label>
-                             By {item.author} {item.date}
-                            </label>
-                            <p>{item.desc}</p>
-                        </div>
-                    </div>
-               )
-                    })}
-            </div>
-            </div>
-        </section>
+      <section className="blog">
+        <div className="container">
+          <Heading title="Blog" />
+          <div className="content grid3">
+            {blog.map((item) => {
+              return (
+                <div className="box fade-in" key={item.id}>
+                  <div className="img">
+                    <img src={item.cover} alt="" />
+                  </div>
+                  <div className="text">
+                    <h3>{item.title}</h3>
+                    <label>
+                      By {item.author} {item.date}
+                    </label>
+                    <p>{item.desc}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
